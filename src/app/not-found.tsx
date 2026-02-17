2efac22a35395d5e53ee0aa322bc75d12e6da6d4
ms19961123 <ms19961123@gmail.com>
2026-02-17 14:29:07 -0500

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-dynamic";
+
+export default function NotFound() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Page not found</h1>
+      <p>The page you are looking for doesn&apos;t exist.</p>
+    </main>
+  );
+}
