0adfd74c57db5fe639cafac3893c1c5eb5445bdf
ms19961123 <ms19961123@gmail.com>
2026-02-17 19:24:20 -0500

    Update page.tsx

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic";
 
-import SignUpClientPage from "./SignUpClientPage";
+import SignUpClientPage from "./SignUpClient";
 
 export default function Page() {
   return <SignUpClientPage />;
